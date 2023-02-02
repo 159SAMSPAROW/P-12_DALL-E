8b820bf8dad5c04368e8ff496ab9b360b61b03bb
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-02 21:22:00 +0100

    backend side implemented

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import { Loader, Card, FormField } from '../components'
+ 
+const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+      return data.map((post) => <Card key={post._id} {...post} />)
+    }
+    return (
+      <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+        {title}
+      </h2>
+    )
+  }
 
 const Home = () => {
   const [loading, setLoading] = useState(false)
   const [allPosts, setAllPosts] = useState(null)
   const [searchText, setSearchText] = useState('azerty')
 
-  const RenderCards = ({ data, title }) => {
-    if (data?.length > 0) {
-      return data.map((post) => <Card key={post._id} {...post} />)
-    } 
-    return (
-      <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
-    )
+  const fetchPosts = async () => {
+    setLoading(true)
+    
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok) {
+        const result = await response.json()
+        setAllPosts(result.data.reverse())
+      }
+    } catch (error) {
+      alert(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  {
+    /* Fonction qui rend des cartes (<Card>) en utilisant les données fournies dans data.
+ Si data est non vide (data?.length > 0), alors la fonction renvoie une liste des cartes
+ (data.map((post) => <Card key={post._id} {...post} />)). 
+Sinon, elle renvoie un en-tête avec un titre spécifié 
+(<h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>). */
   }
 
+ 
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -29,6 +65,12 @@ const Home = () => {
       <div className="mt-16">
         <FormField />
       </div>
+
+      {/* Si "loading" est vrai, il affichera un loader centré à l'écran. Sinon,
+       il affichera un message de résultats avec le texte de recherche si celui-ci est disponible,
+        sinon il affichera un message indiquant qu'aucun post n'a été trouvé.
+         Les résultats seront affichés dans un grid.*/}
+
       <div className="mt-10">
         {loading ? (
           <div className="flex justify-center items-center">
@@ -38,15 +80,15 @@ const Home = () => {
           <>
             {searchText && (
               <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                Affichage des résultats pour
-                <span className="text-[#222328]">{searchText}</span>
+                Affichage des résultats pour 
+                 {' '}<span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
-            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title='Aucun résultat trouvé'/>
+                <RenderCards data={allPosts} title="Aucun résultat trouvé" />
               ) : (
-                <RenderCards data={[]} title='Aucun post trouvé'/>
+                <RenderCards data={[]} title="Aucun post trouvé" />
               )}
             </div>
           </>
---
+++ client/src/utils/index.js
@@ -0,0 +1,16 @@
+import { surpriseMePrompts } from '../constants'
+
+{/* Fonction qui retourne un prompt aléatoire à partir d'une liste de prompts donnée "surpriseMePrompts".
+ Il calcule un index aléatoire en utilisant Math.random() et Math.floor,
+ puis il retourne un prompt correspondant à cet index.
+ Si le prompt aléatoire est égal au prompt donné en argument,
+ il appelle la fonction récursivement jusqu'à ce qu'un prompt différent soit retourné. */}
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
+    const randomPrompt = surpriseMePrompts[randomIndex]
+
+    if (randomPrompt === prompt) return getRandomPrompt(prompt)
+
+    return randomPrompt;
+}
